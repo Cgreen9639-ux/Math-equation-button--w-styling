f733e791a3422b2f062a7ee7b58a3d44324718f9
Cameron Greene <125625082+Cgreen9639-ux@users.noreply.github.com>
2023-05-14 06:38:40 +0000

    Pending changes exported from your codespace

---
+++ app.js
@@ -0,0 +1,12 @@
+document.getElementById('demo').style.color=black
+document.getElementById('demo').DOCUMENT_POSITION_CONTAINED_BY=Animation
+    
+      
+
+       Number(params) 
+        x = 5 
+        y = 6
+        z = x + y
+       
+    
+    
